f6590fd821ee0d311e88cdc5082f690075bf79a0
ahmedvaraliya <ahmedvaraliya38@gmail.com>
2025-02-11 19:11:16 +0530

    project

---
+++ calculator.js
@@ -0,0 +1,16 @@
+alert("Welcome to online computer")
+
+let currentDisplay = '';
+document.querySelector('#flex').value = currentDisplay;
+
+
+let age = 11;
+if (age >= 18) {
+  console.log("you can vote");
+} else if (age<=10) {
+  console.log("Are you child");
+}
+  else {
+  console.log("you cannot vote");
+}
+console.log(age);
